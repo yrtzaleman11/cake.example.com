80fd0ed4fcc485c5fb786328d6c8944aae599ea5
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-23 12:12:31 -0500

    post/users nav bar

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -41,9 +41,13 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+            <!-- <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
+            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a> --> -->
         </div>
+        <a href="/posts/">Posts</a>
+<a href="/users/">Users</a>
+
+
     </nav>
     <main class="main">
         <div class="container">
